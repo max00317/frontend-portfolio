56afd0f0242ac094b91664752700c6cb43438d75
max00317 <passionatedev0317@gmail.com>
2023-04-03 01:37:00 +0800

    remove display error

--- public/assets/vendor/php-email-form/validate.js
+++ public/assets/vendor/php-email-form/validate.js
@@ -18,7 +18,7 @@
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
       
       if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!');
+        // displayError(thisForm, 'The form action property is not set!');
         return;
       }
       thisForm.querySelector('.loading').classList.add('d-block');
